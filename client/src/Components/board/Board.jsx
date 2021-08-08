c1554d9671cd4fd9afd7d2ef01fb8feb19792a81
tushar taral <tushartaral0@gmail.com>
2021-08-08 14:39:49 +0530

    user visible

--- client/src/Components/board/Board.jsx
+++ client/src/Components/board/Board.jsx
@@ -1,71 +1,61 @@
-import React, {useState, useEffect, useRef } from 'react'
-import { useParams } from "react-router-dom"
-import WhiteBoard from "../whiteboard/WhiteBoard"
-import Styles from "./Board.module.css"
-import Axios from 'axios'
-import Boardmembers from "./Boardmembers"
-import Chat from "../chat/Chat/Chat"
-import { useDispatch, useSelector } from "react-redux"
-
+import React, { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import WhiteBoard from "../whiteboard/WhiteBoard";
+import Styles from "./Board.module.css";
+import Axios from "axios";
+import Boardmembers from "./Boardmembers";
+import Chat from "../chat/Chat/Chat";
+import { useDispatch, useSelector } from "react-redux";
 
 function Board() {
+  const { id } = useParams();
+  console.log(id);
 
+  const name = useSelector((state) => state.regi.username);
+  console.log(name);
 
-    const { id } = useParams()
-    console.log(id)
+  const [members, setMembers] = useState([]);
+  const [admin, setAdmin] = useState({});
 
+  useEffect(() => {
+    Axios.get(`http://localhost:5000/groups/${id}`, {}).then((res) => {
+      setMembers(res.data.data.members_id);
+      setAdmin(res.data.data.admin);
+    });
+  }, []);
 
-    const name = useSelector(state => state.regi.username)
-    console.log(name)
+  console.log(members);
 
-    const [members, setMembers] = useState([])
-    const [admin, setAdmin] = useState({})
+  useEffect(() => {
+    const setUser = setInterval(() => {
+      Axios.get(`http://localhost:5000/groups/${id}`, {}).then((res) => {
+        setMembers(res.data.data.members_id);
+        setAdmin(res.data.data.admin);
+      });
+    }, 2000);
+  }, []);
 
-    
-    
- 
-    useEffect(() => {
-       
-        Axios.get(`http://localhost:5000/groups/${id}`,{
-
-        }).then((res) => {
-            setMembers(res.data.data.members_id)
-            setAdmin(res.data.data.admin)
-        })
-
-    }, [])
-
-    console.log(members)
-
-
-  
-
-    return (
-        <div>
-<div className={Styles.board}>
-           
-            <div className={Styles.left}>
-                 <WhiteBoard />
-            </div>
-           
-            <div className={Styles.right}>
-                 <div className={Styles.righttop}>
-                      <Boardmembers admin={admin} members={members}/>
-                 </div>
-
-                 <div className={Styles.rightmid}>
-                    <Chat room1={id} name1={name}/>
-                </div>
-
-                 <div className={Styles.rightbtm}>
-
-                 </div>
-            </div>
-
+  return (
+    <div>
+      <div className={Styles.board}>
+        <div className={Styles.left}>
+          <WhiteBoard />
         </div>
+
+        <div className={Styles.right}>
+          <div className={Styles.righttop}>
+            <Boardmembers admin={admin} members={members} />
+          </div>
+
+          <div className={Styles.rightmid}>
+            <Chat room1={id} name1={name} />
+          </div>
+
+          <div className={Styles.rightbtm}></div>
         </div>
-        
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Board;
